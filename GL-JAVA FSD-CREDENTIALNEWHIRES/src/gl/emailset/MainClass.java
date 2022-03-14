20ceebce5b3858640de32517fe02c5c608bfd9ef
Abhikgit <99563359+Abhikgit@users.noreply.github.com>
2022-03-14 23:11:00 +0530

    Add files via upload

---
+++ GL-JAVA FSD-CREDENTIALNEWHIRES/src/gl/emailset/MainClass.java
@@ -0,0 +1,11 @@
+package gl.emailset;
+import glemail.ui.EmailApplicationUI;
+
+public class MainClass {
+	public static void main(string[] args) {
+		EmailApplicationUI ui = new EmailApplicationUI();
+		
+		ui.init();
+	}
+
+}
